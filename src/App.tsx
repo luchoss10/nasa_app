973bd010e12f07e0fe808ca182809045d1573afb
Lucho <lucncd@gmail.com>
2024-10-10 23:40:47 -0500

    feat: rovers api limit added, datepicker implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import {Button}from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs, { Dayjs } from 'dayjs';
 
 // const API_KEY = 'DEMO_KEY';
 const API_KEY = process.env.REACT_APP_NASA_API_KEY;
-console.log('API_KEY', API_KEY);
-const APOD_API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5`;
-// const ROVER_API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`;
+const APOD_API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+const ROVER_API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=${API_KEY}`;
 
 interface Photo {
   title: string;
@@ -23,12 +23,31 @@ function App() {
 
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [isRoverPhotos, setIsRoverPhotos] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+  
+  console.log(selectedDate?.format('YYYY-MM-DD'));
 
   useEffect(() => {
     const fetchPhotos = async () => {
       try {
-        const response = await fetch(isRoverPhotos ? APOD_API_URL : APOD_API_URL);
+        
+        let url:string = isRoverPhotos ? ROVER_API_URL : APOD_API_URL;
+        if (selectedDate) {
+            const formattedDate:string = selectedDate.format('YYYY-MM-DD');
+            const lastYearFormattedDate = selectedDate.subtract(1, 'month').format('YYYY-MM-DD');
+            
+            url = isRoverPhotos ? `${url}&earth_date=${formattedDate}` : `${url}&end_date=${formattedDate}&start_date=${lastYearFormattedDate}`;
+        } else {
+            const today = dayjs().format('YYYY-MM-DD');
+            url = isRoverPhotos ? `${url}&earth_date=${today}` : `${url}&count=5`;
+        }
+        console.log(url);
+
+        const response = await fetch(url);
         const data = await response.json();
+
+        console.log(data);
+
         if (isRoverPhotos) {
           setPhotos(data.photos.slice(0, 5).map((photo: any) => ({
             title: photo.camera.full_name,
@@ -48,7 +67,7 @@ function App() {
   }
   fetchPhotos();
   }
-  , [isRoverPhotos]);
+  , [isRoverPhotos, selectedDate]);
 
 const toggleEndpoint = () => {
   setIsRoverPhotos(!isRoverPhotos);
@@ -63,7 +82,11 @@ const toggleEndpoint = () => {
           {isRoverPhotos ? 'APOD' : 'Curiosity Rover'}
         </Button>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker />
+            <DatePicker 
+                label="Date Selector" 
+                value={selectedDate}
+                onChange={setSelectedDate} 
+                />
         </LocalizationProvider>
         </div>
       </header>
